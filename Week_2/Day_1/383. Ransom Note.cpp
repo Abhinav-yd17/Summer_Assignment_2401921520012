97271329e417ad08c0ba84192f1a9a4e8b72385a
Abhinav Yadav <abhinavyadav.ay17@gmail.com>
2026-06-08 20:44:24 +0530

    changes

---
+++ Week_2/Day_1/383. Ransom Note.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char,int> mp;
+
+        for(char ch : magazine)
+        {
+            mp[ch]++;
+        }
+
+        for(char ch : ransomNote)
+        {
+            mp[ch]--;
+
+            if(mp[ch] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
